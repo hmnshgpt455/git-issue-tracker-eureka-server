6630d3e85059dc2df216d19f13faefe2b360e41f
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-05-14 10:01:53 +0530

    Adding initial implementation for eureka server. Closes #1.

---
+++ src/test/java/io/github/hmnshgpt455/gitissuetrackereurekaserver/GitIssueTrackerEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hmnshgpt455.gitissuetrackereurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIssueTrackerEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
